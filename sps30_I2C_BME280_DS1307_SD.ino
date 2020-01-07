133ac4e850d52c2b7ae2e2a54b96b29a4860f5db
goeselyceum <55946447+goeselyceum@users.noreply.github.com>
2020-01-07 11:33:39 +0100

    Update sps30_I2C_BME280_DS1307_SD.ino

--- sps30_I2C_BME280_DS1307_SD.ino
+++ sps30_I2C_BME280_DS1307_SD.ino
@@ -30,7 +30,7 @@ void setup() {
   if (! rtc.isrunning()) {
     Serial.println("RTC is NOT running!");
     // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)))
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
   }
 
   bme.begin();
